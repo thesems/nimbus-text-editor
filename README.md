0e2e80fece457b2ba17b30274383c97877bb17cc
Semir Ramovic <semir.ramovic@easelink.com>
2024-01-15 12:11:15 +0100

    Init.

---
+++ README.md
@@ -0,0 +1,7 @@
+## Nimbus Text Editor
+
+Nimbus is a terminal-based text editor. It is a personal study project and is based on the hecto editor.
+
+### References:
+[Hector editor in Rust](https://www.flenker.blog/hecto/)  
+[Kilo editor in C](https://viewsourcecode.org/snaptoken/kilo/index.html)
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,22 @@
-use gtk4 as gtk;
-use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow, Button};
+use termion::{event::Key, input::TermRead, raw::IntoRawMode};
+use std::io::{self, stdin, stdout, Write};
 
-const APP_ID: &str = "org.nimbus.NimbusTextEditor";
+fn main() -> Result<(), io::Error> {
+    let mut stdout = stdout().into_raw_mode()?;
 
-fn main() -> glib::ExitCode {
-    // Create a new application
-    let app = Application::builder().application_id(APP_ID).build();
-
-    // Connect to "activate" signal of `app`
-    app.connect_activate(build_ui);
-
-    // Run the application
-    app.run()
-}
-
-fn build_ui(app: &Application) {
-    // Create a button with label and margins
-    let button = Button::builder()
-        .label("Press me!")
-        .margin_top(12)
-        .margin_bottom(12)
-        .margin_start(12)
-        .margin_end(12)
-        .build();
-
-    // Connect to "clicked" signal of `button`
-    button.connect_clicked(|button| {
-        // Set the label to "Hello World!" after the button has been clicked on
-        button.set_label("Hello World!");
-    });
-
-    // Create a window
-    let window = ApplicationWindow::builder()
-        .application(app)
-        .title("My GTK App")
-        .child(&button)
-        .build();
-
-    // Present window
-    window.present();
+    let stdin = stdin();
+    for key_opt in stdin.keys() {
+        let key = key_opt.unwrap();
+        match key {
+            Key::Char(c) => {
+                if c == 'q' {
+                    return Ok(());
+                }
+                print!("{}", c);
+                stdout.flush().unwrap();
+            }
+            _ => {}
+        }
+    }
+    Ok(())
 }
